f1896606449ae78dec00eb15dafadee6e5aafc59
raja <rajababuchauhan500@gmail.com>
2025-08-01 14:43:59 +0200

    bug fixes

--- src/MotivationAndCVmakerBasedOnJob.py
+++ src/MotivationAndCVmakerBasedOnJob.py
@@ -8,8 +8,8 @@ import json
 import hashlib
 
 
-MODEL = "gemma3:4b"
-REASONING_MODEL = "deepseek-r1:1.5b"
+MODEL = "gemma3n:e4b" # gemma3:4b deepseek-r1:1.5b deepseek-r1:7b
+TEMPERATURE = 0.2
 
 class AgentState(TypedDict):
     task_id: str
@@ -20,67 +20,55 @@ class AgentState(TypedDict):
     cvContent: str
     revise_cv: Literal["approve", "revise"]
 
-class Log:
-    @staticmethod
-    def input(agent_state, data, func_name= "-no-name-", var_name= "-no-name-"):
-        Log.store(agent_state, data, func_name, var_name, "input")
-    @staticmethod
-    def store(agent_state, data, func_name= "-no-name-", var_name= "-no-name-", typ="output"):
+class DbWrapper:
+    inst = None
+    def read_or_create(self, task_id, func, func_name= "-no-name-", var_name= "-no-name-", more_info={}):
+        val = self.read(task_id, func_name, var_name)
+        if val is None:
+            val = func()
+            self.create(task_id, val, func_name, var_name, more_info )
+            return val
+        return val.data
+    def _run_command(self, func, *params, **kwargs):
         db.connect()
-        LLMAgentData.create(task_id=agent_state['task_id'], 
+        try:
+            res = func(*params, **kwargs)
+        except Exception as e:
+            print(f"Error : {e}")
+            res = None
+        finally:
+            db.close()
+        return res
+    def create(self, task_id, data, func_name= "-no-name-", var_name= "-no-name-", more_info ={}):
+        return self._run_command(LLMAgentData.create, task_id=task_id, 
             function_name=func_name, 
-            type=typ, 
             variable_name=var_name, 
-            data=data)
-        db.close()
-    @staticmethod
-    def output(agent_state, data, func_name= "-no-name-", var_name= "-no-name-"):
-        Log.store(agent_state, data, func_name, var_name, "output")
-    @staticmethod
-    def process(agent_state, data, func_name= "-no-name-", var_name= "-no-name-"):
-        Log.store(agent_state, data, func_name, var_name, "process")
+            data=data, more_info=more_info)
+    def read(self, task_id, func_name= "-no-name-", var_name= "-no-name-"):
+        return self._run_command(LLMAgentData.get_or_none, task_id=task_id, 
+            function_name=func_name, 
+            variable_name=var_name)
 
 class ReadLog:
+    inst = None
     @staticmethod
-    def read(agent_state, func_name= "-no-name-", var_name= "-no-name-", typ="output"):
-        db.connect()
-        entry = LLMAgentData.get(task_id=agent_state['task_id'], 
-            function_name=func_name, 
-            type=typ, 
-            variable_name=var_name)
-        db.close()
-        return entry
+    def get_instance():
+        if ReadLog.inst is None:
+            ReadLog.inst = DbWrapper()
+        return ReadLog.inst
     @staticmethod
-    def output(agent_state, func_name= "-no-name-", var_name= "-no-name-"):
-        return ReadLog.read(agent_state, func_name, var_name, "output")
-    @staticmethod
-    def exists(agent_state, func_name= "-no-name-", var_name= "-no-name-", typ="output"):
-        db.connect()
-        entry = LLMAgentData.get_or_none(task_id=agent_state['task_id'], 
-            function_name=func_name, 
-            type=typ, 
-            variable_name=var_name)
-        db.close()
-        return entry is not None
-    @staticmethod
-    def readOrCreate(agent_state,func, func_name= "-no-name-", var_name= "-no-name-", typ="output"):
-        db.connect()
-        entry = LLMAgentData.get_or_none(task_id=agent_state['task_id'], 
-            function_name=func_name, 
-            type=typ, 
-            variable_name=var_name)
-        db.close()
-        if entry is None:
-            entry = func()
-            Log.store(agent_state, entry, func_name, var_name, "output")
-        return entry
+    def readOrCreate(task_id, func, func_name= "-no-name-", var_name= "-no-name-", more_info={}):
+        inst = ReadLog.get_instance()
+        return inst.read_or_create(task_id, func, func_name, var_name, more_info)
 
 def runModel(model, prompt, state, func_name= "-no-name-"):
-    newPrompt = ReadLog.readOrCreate(state, lambda: prompt, func_name, "prompt", "process")
-    llm = ChatOllama(model=model, temperature=0.7)
-    response = ReadLog.readOrCreate(state, lambda: llm.invoke([HumanMessage(content=newPrompt)]), func_name, "response")
+    newPrompt = ReadLog.readOrCreate(state["task_id"], lambda: prompt, func_name, "prompt")
+    llm = ChatOllama(model=model, temperature=TEMPERATURE)
+    key = generate_key((state["task_id"], model, TEMPERATURE))
+    response = ReadLog.readOrCreate(key, lambda: llm.invoke([HumanMessage(content=newPrompt)]).content, 
+        func_name, "response", {"model":model, "temperature":TEMPERATURE})
     return response
-
+    
 def relevant_content_matcher(state: AgentState) -> AgentState:
     prompt = f"""
     Given below is the summary of job description
@@ -93,8 +81,8 @@ def relevant_content_matcher(state: AgentState) -> AgentState:
 
     list the relevant content as bullet points
     """
-    response = runModel(MODEL, prompt, state, inspect.currentframe().f_code.co_name)
-    return {"relevant_content": response.content}
+    response_content = runModel(MODEL, prompt, state, inspect.currentframe().f_code.co_name)
+    return {"relevant_content": response_content}
 
 def job_description_summarizer(state: AgentState) -> AgentState:
     prompt = f"""
@@ -103,8 +91,8 @@ def job_description_summarizer(state: AgentState) -> AgentState:
 
     Give a summary of the job description (skills, experience, etc. things that are relevant to job application)
     """
-    response = runModel(MODEL, prompt, state, inspect.currentframe().f_code.co_name)
-    return {"job_summarization": response.content}
+    response_content = runModel(MODEL, prompt, state, inspect.currentframe().f_code.co_name)
+    return {"job_summarization": response_content}
 
 def motivation_writer(state: AgentState) -> AgentState:
     prompt = f"""
@@ -116,8 +104,8 @@ def motivation_writer(state: AgentState) -> AgentState:
 
     Write a motivation letter for the candidate
     """
-    response = runModel(MODEL, prompt, state, inspect.currentframe().f_code.co_name)
-    return {"motivation": response.content}
+    response_content = runModel(MODEL, prompt, state, inspect.currentframe().f_code.co_name)
+    return {"motivation": response_content}
 
 def cv_writer(state: AgentState) -> AgentState:
     prompt = f"""
@@ -129,8 +117,8 @@ def cv_writer(state: AgentState) -> AgentState:
 
     make a CV for the candidate
     """
-    response = runModel(REASONING_MODEL, prompt, state, inspect.currentframe().f_code.co_name)
-    return {"cvContent": response.content}
+    response_content = runModel(MODEL, prompt, state, inspect.currentframe().f_code.co_name)
+    return {"cvContent": response_content}
 
 def review_node(state: AgentState) -> AgentState:
     """Asks for human approval"""
@@ -140,7 +128,8 @@ def review_node(state: AgentState) -> AgentState:
     print("\n📝 cv content:")
     print(state["cvContent"])
     feedback = input("\nApprove? (yes/no): ").strip().lower()
-    return {"revise_cv": "approve" if feedback == "yes" else "revise"}
+    res = {"revise_cv": "approve" if feedback == "yes" else "revise"}
+    return res
 
 def should_revise(state: AgentState):
     return "revise" if state["revise_cv"] == "revise" else END
@@ -171,6 +160,7 @@ def generate_key(params) -> str:
     serialized_params = json.dumps(params, sort_keys=True, separators=(',', ':'))
     hash_object = hashlib.sha256(serialized_params.encode('utf-8'))
     return hash_object.hexdigest()
+
 def job_apply_helper(candidate_profile, job_description):
     key = generate_key((candidate_profile.strip(), job_description.strip()))
     graph = get_pipeline_graph()

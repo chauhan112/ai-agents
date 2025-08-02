d6dda986eeb9a09906ffe46c8e04e38c192218b7
raja <rajababuchauhan500@gmail.com>
2025-08-02 14:52:05 +0200

    new tests

--- main.py
+++ main.py
@@ -11,8 +11,25 @@ from src.rlib.useful.FileDatabase import File
 job_content = File.getFileContent("./tests/job_apply_helper/job_description.txt").strip()
 candidate_profile = File.getFileContent("./tests/job_apply_helper/candidate_profile.txt").strip()
 #%%
-job_apply_helper(candidate_profile, job_content)
+job_apply_helper(candidate_profile, job_content, 
+    outCV="output/cv.txt", 
+    outMotivation="output/motivation.txt", redo=True)
+
+#%%
+from src.db.dbInfo import db, LLMAgentData
+
+vals = LLMAgentData.select()
+for val in vals:
+    print(val.task_id, val.function_name, val.variable_name, val.more_info)
+
 # %%
+
+
+
+
+
+
+
 key = generate_key((candidate_profile, job_content))
 
 state = {"task_id": key,"job_description": job_content, "candidate_profile_info": candidate_profile, "revise_cv": ""}
@@ -25,7 +42,31 @@ from src.db.dbInfo import db, LLMAgentData
 
 vals = LLMAgentData.select()
 for val in vals:
-    print(val.task_id, val.function_name, val.type, val.variable_name, val.data)
+    print(val.task_id, val.function_name, val.variable_name)
+# %%
+# ReaLog.readOrCreate({"task_id": val.task_id,"job_ddescription": job_content, "candidate_profile_info": candidate_profile, "revise_cv": ""}, lambda: job_content, "func_n", "job_description", "input")
+
+db.connect()
+
+
+res =LLMAgentData.get_or_none(task_id="1384cbbd9e0359f38a29d521c5fb4632d61e07806fc8165e389971a96506cc5e",
+    function_name="job_description_summarizer",
+    variable_name="response"
+)
+
+
+
+db.close()
+
+
+# ReadLog.readOrCreate(
+#     agent_state={"task_id": "1384cbbd9e0359f38a29d521c5fb4632d61e07806fc8165e389971a96506cc5e"},
+#     func = lambda: "test",
+#     func_name="job_description_summarizer",
+#     var_name="response",
+#     typ="output"
+# )
+
 # %%
 prompt = f"""
 Given below is the job description
